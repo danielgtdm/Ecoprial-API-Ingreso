e3486d5e2f72ed66901d829b17a1ba445f8e432e
danielgtdm <daniel.gtmd@gmail.com>
2021-02-27 12:14:21 -0300

    add config module and configure debuger

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors();
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+    next();
+  });
+  app.setGlobalPrefix('api');
+  await app.listen(AppModule.port);
 }
 bootstrap();
