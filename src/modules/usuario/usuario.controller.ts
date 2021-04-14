bdf8a4b67dc43f1045670d3c7c89db93376ebbda
danielgtdm <daniel.gtmd@gmail.com>
2021-04-13 21:23:54 -0400

    enable auth

--- src/modules/usuario/usuario.controller.ts
+++ src/modules/usuario/usuario.controller.ts
@@ -15,8 +15,8 @@ import { RolGuard } from '../rol/guards/rol.guard';
 import { Usuario } from './usuario.entity';
 import { UsuarioService } from './usuario.service';
 
-//@Roles('ADMINISTRADOR')
-//@UseGuards(AuthGuard(), RolGuard)
+@Roles('ADMINISTRADOR')
+@UseGuards(AuthGuard(), RolGuard)
 @Controller('usuario')
 export class UsuarioController {
   constructor(private readonly _usuarioService: UsuarioService) {}
